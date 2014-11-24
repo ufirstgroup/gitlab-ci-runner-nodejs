50eb938a359e8af5daba742e867ed00798ed692a
brownman <brownman556@gmail.com>
2014-11-24 19:11:56 +0200

    validate existance of required ENV VARS

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -u
-
+set -e
+test -v CI_SERVER_URL
+test -v REGISTRATION_TOKEN
+test -v GITLAB_SERVER_FQDN
 
 #sudo apt-get purge docker.io
 curl -s https://get.docker.io/ubuntu/ | sudo sh
