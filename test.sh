2de3b23aee4b6c0f6c5653e56936f7c5f282d3e3
brownman <brownman556@gmail.com>
2014-11-24 18:27:27 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 set -u
 
 
-sudo apt-get purge docker.io
+#sudo apt-get purge docker.io
 curl -s https://get.docker.io/ubuntu/ | sudo sh
 sudo apt-get update
 sudo apt-get install lxc-docker
