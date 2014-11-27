ed7f448e7ec687d5d4ce101569917b6ac16ebb1d
brownman <brownman556@gmail.com>
2014-11-27 18:15:13 +0200

    install ruby

--- SH/ruby.sh
+++ SH/ruby.sh
@@ -1,10 +1,10 @@
 
 # Download Ruby and compile it
-RUN mkdir /tmp/ruby && cd /tmp/ruby && curl -s http://ftp.ruby-lang.org/pub/ruby/ruby-2.0-stable.tar.bz2 | tar xj --strip-components=1
-RUN cd /tmp/ruby && ./configure --disable-install-rdoc --silent && make && make install
-RUN rm -rf /tmp/ruby
+  mkdir /tmp/ruby && cd /tmp/ruby && curl -s http://ftp.ruby-lang.org/pub/ruby/ruby-2.0-stable.tar.bz2 | tar xj --strip-components=1
+  cd /tmp/ruby && ./configure --disable-install-rdoc --silent && make && make install
+  rm -rf /tmp/ruby
 
 # don't install ruby rdocs or ri:
-RUN echo "gem: --no-rdoc --no-ri" >> /usr/local/etc/gemrc
+  echo "gem: --no-rdoc --no-ri" >> /usr/local/etc/gemrc
 
 
