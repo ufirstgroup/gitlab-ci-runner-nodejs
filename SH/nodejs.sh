f37d3aa09fa4835c1e486c5695a132cead5760f1
brownman <brownman556@gmail.com>
2014-11-27 17:34:36 +0200

    update npm + install some basic packages

--- SH/nodejs.sh
+++ SH/nodejs.sh
@@ -6,3 +6,7 @@ cat /root/.bashrc /.nvm/nvm.sh
 bash -c '. /.nvm/nvm.sh ; nvm install 0.10'
 #bash -c '. /.nvm/nvm.sh ; nvm install 0.11'
 bash -c '. /.nvm/nvm.sh ; nvm alias default 0.10'
+
+# update npm and install some basics
+ npm update -g npm
+ npm install -g grunt grunt-cli bower jshint jsxhint
