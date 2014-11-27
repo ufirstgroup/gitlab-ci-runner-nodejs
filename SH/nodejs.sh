e1a60b27388c4b8592a18159ed087226bff5db8a
brownman <brownman556@gmail.com>
2014-11-27 17:33:16 +0200

    install nodejs

---
+++ SH/nodejs.sh
@@ -0,0 +1,8 @@
+
+wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+echo '. /.nvm/nvm.sh' >> /root/.bashrc
+cat /root/.bashrc /.nvm/nvm.sh
+
+bash -c '. /.nvm/nvm.sh ; nvm install 0.10'
+#bash -c '. /.nvm/nvm.sh ; nvm install 0.11'
+bash -c '. /.nvm/nvm.sh ; nvm alias default 0.10'
