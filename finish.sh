6235aab410260e7b7ab89adb6a2ea437c7159ae6
brownman <brownman556@gmail.com>
2014-11-30 16:28:37 +0200

    why mean is so great ?

--- finish.sh
+++ finish.sh
@@ -5,7 +5,7 @@
 
 cd /tmp/myApp
 grunt &
-while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
+while true; do  curl 0.0.0.0:3000 | grep FullStack  2>/dev/null && break ; sleep 1 ; done
 
 
 #mean search gmap
