5b26a250274e7f9c9d25f48164bd2937dbe59b79
brownman <brownman556@gmail.com>
2014-11-30 14:54:57 +0200

    run grunt
    [skip ci]

--- SH/meanio.sh
+++ SH/meanio.sh
@@ -3,7 +3,7 @@
 echo Install the MEAN CLI
 npm install -g mean-cli
 mean init myApp
-cd myApp && npm install
-echo Invoke node with Grunt
-echo We recommend using Grunt to start the server:
-echo run grunt!
+cd myApp 
+npm install -g
+npm link
+grunt &
