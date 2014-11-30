78980e122066b58c3dcab62b5667fdf0366ee06e
brownman <brownman556@gmail.com>
2014-11-30 11:46:43 +0200

    mv code to steps.txt

--- docker_steps.sh
+++ docker_steps.sh
@@ -1,30 +1,32 @@
 #ADD: copies new files, directories or remote file URLs from <src> and adds them to the filesystem of the container at the path <dest>. 
 
- step(){
+ commander(){
  local args=( $@ ) 
  local cmd="${args[@]}"
  echo "[CMD] $cmd"
  eval "$cmd" 1>/tmp/out 2>/tmp/err || { exit 1; }
  }
-
-#update permission for our scripts
- step permissions chmod +x ./SH/*.sh
-##################################
-
-  #enable sudo command
- step sudoers ./SH/sudoers.sh
-
-
-  #install node through nvm
- step nodejs ./SH/nodejs.sh
-
-  #try installing mongodb using external file
- step mongodb ./SH/mongodb.sh
-
-  #try installing meanio dependencies
- step mean-framework ./SH/meanio.sh
-
-
- step mean-packabe ./SH/mean_add_package.sh
  
+breaking(){
+ echo 
+ echo Empty line!
+ echo
+ echo breaking..
+ break
+}
+
+run(){
+local desc cmd
  
+while read line;do
+ test -n "$line" || breaking
+ desc=$(  echo $line | cut -d':' -f1 )
+ cmd=$(  echo $line | cut -d':' -f2- )
+ 
+ echo "[DESC] $desc"
+ commander $cmd
+done < <(cat steps.txt)
+}
+
+run
+return 0
