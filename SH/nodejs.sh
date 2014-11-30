e068cb2d7cb36e3c8d8babcaa922fcf20fa24e54
brownman <brownman556@gmail.com>
2014-11-30 12:54:12 +0200

    only install nodejs
    [skip ci]

--- SH/nodejs.sh
+++ SH/nodejs.sh
@@ -1,12 +1,9 @@
 
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 echo '. /.nvm/nvm.sh' >> /root/.bashrc
-cat /root/.bashrc /.nvm/nvm.sh
+cat $HOME/.bashrc /.nvm/nvm.sh
 
 bash -c '. /.nvm/nvm.sh ; nvm install 0.10'
 #bash -c '. /.nvm/nvm.sh ; nvm install 0.11'
 bash -c '. /.nvm/nvm.sh ; nvm alias default 0.10'
 
-# update npm and install some basics
- npm update -g npm
- npm install -g grunt grunt-cli bower jshint jsxhint
