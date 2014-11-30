d9a91d439eb3a64e6e88df6c1205c9bd65ef26dc
brownman <brownman556@gmail.com>
2014-11-30 11:32:14 +0200

    mute noise

--- docker_steps.sh
+++ docker_steps.sh
@@ -1,20 +1,30 @@
 #ADD: copies new files, directories or remote file URLs from <src> and adds them to the filesystem of the container at the path <dest>. 
 
- 
+ step(){
+ local args=( $@ ) 
+ local cmd="${args[@]}"
+ echo "[CMD] $cmd"
+ eval "$cmd" 1>/tmp/out 2>/tmp/err || { exit 1; }
+ }
 
 #update permission for our scripts
-  chmod +x ./SH/*.sh
+ step permissions chmod +x ./SH/*.sh
 ##################################
 
   #enable sudo command
-  ./SH/sudoers.sh
+ step sudoers ./SH/sudoers.sh
 
 
   #install node through nvm
-  ./SH/nodejs.sh
+ step nodejs ./SH/nodejs.sh
 
   #try installing mongodb using external file
-  ./SH/mongodb.sh
+ step mongodb ./SH/mongodb.sh
 
   #try installing meanio dependencies
-  ./SH/meanio.sh
+ step mean-framework ./SH/meanio.sh
+
+
+ step mean-packabe ./SH/mean_add_package.sh
+ 
+ 
