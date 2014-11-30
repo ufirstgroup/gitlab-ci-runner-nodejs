eed42ff20f8acbc54d8e31f77941a471e95d4ff4
brownman <brownman556@gmail.com>
2014-11-30 14:03:36 +0200

    are u failing ?

--- SH/mongodb.sh
+++ SH/mongodb.sh
@@ -1,10 +1,11 @@
+set -e
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
 apt-get update && apt-get upgrade -y
 apt-get install git python build-essential wget screen tmux curl vim mongodb-org -y
-sudo service mongod start
+service mongod start
 
 mkdir /data
 mkdir /data/db
