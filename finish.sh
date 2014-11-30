318c3f1c8533ad77a6a141af20bb91b44742d833
brownman <brownman556@gmail.com>
2014-11-30 16:27:35 +0200

    only run grunt

--- finish.sh
+++ finish.sh
@@ -3,16 +3,8 @@
 #grunt
 
 
-cd /tmp
-mean init myApp
-cd myApp 
-npm install -g
-npm link
+cd /tmp/myApp
 grunt &
-
-
-
-
 while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
 
 
