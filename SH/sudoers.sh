6b59e2e7e08078657f60a2f044f1b344399f73b3
brownman <brownman556@gmail.com>
2014-11-30 15:32:57 +0200

    update package index
    then find the package: sudo

--- SH/sudoers.sh
+++ SH/sudoers.sh
@@ -1,4 +1,5 @@
 #ALLOW USING SUDO
+apt-get update -y
 apt-get install sudo
 useradd docker && echo "docker:docker" | chpasswd && adduser docker sudo
 mkdir -p /home/docker && chown -R docker:docker /home/docker
