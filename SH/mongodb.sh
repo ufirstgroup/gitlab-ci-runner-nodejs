1a8293b780bf7e5716892664ff770bb63fe0017a
brownman <brownman556@gmail.com>
2014-11-30 13:23:55 +0200

    try fix permission

--- SH/mongodb.sh
+++ SH/mongodb.sh
@@ -1,6 +1,6 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
 apt-get update && apt-get upgrade -y
 apt-get install git python build-essential wget screen tmux curl vim mongodb-org -y
