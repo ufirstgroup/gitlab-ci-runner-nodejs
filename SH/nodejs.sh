af680257246848cc11e0b7d6e29fbb811eaa6641
brownman <brownman556@gmail.com>
2014-11-30 13:13:57 +0200

    fix nvm loading

--- SH/nodejs.sh
+++ SH/nodejs.sh
@@ -1,9 +1,9 @@
 
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
-echo '. /.nvm/nvm.sh' >> /root/.bashrc
-cat $HOME/.bashrc /.nvm/nvm.sh
-
-bash -c '. /.nvm/nvm.sh ; nvm install 0.10'
+echo '. $HOME/.nvm/nvm.sh' >> $HOME/.bashrc
+#cat $HOME/.bashrc /.nvm/nvm.sh
+source $HOME/.bashrc
+bash -c '. $HOME/.nvm/nvm.sh ; nvm install 0.10'
 #bash -c '. /.nvm/nvm.sh ; nvm install 0.11'
-bash -c '. /.nvm/nvm.sh ; nvm alias default 0.10'
+bash -c '. $HOME/.nvm/nvm.sh ; nvm alias default 0.10'
 
