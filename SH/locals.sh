7662130cc5805fa35f049fba1f5bbcb675b4254f
brownman <brownman556@gmail.com>
2014-12-01 18:07:49 +0200

    fix: permission

--- SH/locals.sh
+++ SH/locals.sh
@@ -1,5 +1,5 @@
 
 # Set the right locale
-  echo "LC_ALL=\"en_US.UTF-8\"" | sudo tee /etc/default/locale
-  locale-gen en_US.UTF-8
-  update-locale LANG=en_US.UTF-8
+echo "LC_ALL=\"en_US.UTF-8\"" | sudo tee /etc/default/locale
+sudo locale-gen en_US.UTF-8
+sudo  update-locale LANG=en_US.UTF-8
