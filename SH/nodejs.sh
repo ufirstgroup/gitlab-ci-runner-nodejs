c2f5797d14eac63f4216615baeb8b8a9463cb52d
brownman <brownman556@gmail.com>
2014-12-01 17:33:11 +0200

    Update nodejs.sh

--- SH/nodejs.sh
+++ SH/nodejs.sh
@@ -3,11 +3,11 @@ test -v dir_bashrc
 test -v dir_nvm
 
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
-echo ". $dir_nvm/.nvm/nvm.sh" >> $dir_bashrc/.bashrc
+echo ". ${dir_nvm}/.nvm/nvm.sh" >> $dir_bashrc/.bashrc
 #cat $HOME/.bashrc /.nvm/nvm.sh
 source $dir_bashrc/.bashrc
 . $dir_nvm/.nvm/nvm.sh
 nvm install 0.10
-. $dir_nvm/.nvm/nvm.sh
+#. $dir_nvm/.nvm/nvm.sh
 nvm alias default 0.10
 
