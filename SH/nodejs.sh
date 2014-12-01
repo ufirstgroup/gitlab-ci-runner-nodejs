df91688bf4a8878b011a680e1428e374d3d50b19
brownman <brownman556@gmail.com>
2014-12-01 16:51:02 +0200

    fix: bash-quotes issue
    dir_nvm defined after loading of .bashrc

--- SH/nodejs.sh
+++ SH/nodejs.sh
@@ -1,9 +1,13 @@
 
+test -v dir_bashrc
+test -v dir_nvm
+
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
-echo '. $dir_nvm/.nvm/nvm.sh' >> $dir_bashrc/.bashrc
+echo ". $dir_nvm/.nvm/nvm.sh" >> $dir_bashrc/.bashrc
 #cat $HOME/.bashrc /.nvm/nvm.sh
-source $dir_nvm/.bashrc
-bash -c '. $dir_nvm/.nvm/nvm.sh ; nvm install 0.10'
-#bash -c '. /.nvm/nvm.sh ; nvm install 0.11'
-bash -c '. $dir_nvm/.nvm/nvm.sh ; nvm alias default 0.10'
+source $dir_bashrc/.bashrc
+. $dir_nvm/.nvm/nvm.sh
+nvm install 0.10
+. $dir_nvm/.nvm/nvm.sh
+nvm alias default 0.10
 
