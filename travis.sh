158d7a7a7650a664b5dfc2e2986a0e504ea8aca7
brownman <brownman556@gmail.com>
2014-12-01 13:48:18 +0200

    [avoid spaghetti] - from the start use: ENV VARS
    [skip ci]

--- travis.sh
+++ travis.sh
@@ -1,4 +1,14 @@
-chmod u+x *.sh . -R
+trap_err(){
+  echo $FUNCNAME
+  echo $?
+  exit 1
+}
+
+set -u
+trap trap_err ERR
+
+source $ROOT/config.cfg
+chmod u+x $ROOT/*.sh . -R
 sudo -E su $LOGNAME -c 'echo hello world'
-bash -c ./steps.sh
-bash -c ./finish.sh
+bash -c $ROOT/steps.sh
+bash -c $ROOT/finish.sh
