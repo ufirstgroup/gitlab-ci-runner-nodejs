60ce5af80a151344323e630c2695aed8e2430c4f
brownman <brownman556@gmail.com>
2014-12-01 13:01:47 +0200

    Create apt2.sh

---
+++ EXTRA/apt2.sh
@@ -0,0 +1,11 @@
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv E1DF1F24 \
+ && echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main" >> /etc/apt/sources.list \
+ && apt-key adv --keyserver keyserver.ubuntu.com --recv C3173AA6 \
+ && echo "deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main" >> /etc/apt/sources.list \
+ && apt-get update \
+ && apt-get install -y supervisor git-core openssh-client ruby2.1 \
+      zlib1g libyaml-0-2 libssl1.0.0 \
+      libgdbm3 libreadline6 libncurses5 libffi6 \
+      libxml2 libxslt1.1 libcurl3 libicu52 \
+&& gem install --no-document bundler \
+&& rm -rf /var/lib/apt/lists/* # 20140918
