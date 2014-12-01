cacc1beca2a50e5ce7ce7c6ef3ccba0fb88d9b5d
brownman <brownman556@gmail.com>
2014-12-01 16:05:17 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -8,8 +8,15 @@ set -u
 trap trap_err ERR
 export dir_root=$PWD
 #{dir_root:-$PWD}
+
+#LOAD
+source $dir_root/CFG/helper.cfg
 source $dir_root/CFG/config.cfg
+
+
 chmod u+x $dir_root/*.sh . -R
+
+#RUN
 sudo -E su $USER_ADMIN -c 'echo hello world'
 bash -c $dir_root/steps.sh
 bash -c $dir_root/finish.sh
