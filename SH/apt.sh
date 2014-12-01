c508ec2726f927d2a037796e721ca5778b4b4bb6
brownman <brownman556@gmail.com>
2014-12-01 16:55:31 +0200

    Update apt.sh

--- SH/apt.sh
+++ SH/apt.sh
@@ -1,29 +1,6 @@
 # Update your packages and install the ones that are needed to compile Ruby
   sudo apt-get update -y
-  sudo apt-get install -y \
-  wget \
-  curl \
-  gcc \
-  libxml2-dev \
-  libxslt-dev \
-  libcurl4-openssl-dev \
-  libreadline6-dev \
-  libc6-dev \
-  libssl-dev \
-  make \
-  build-essential \
-  zlib1g-dev \
-  openssh-server \
-  git-core \
-  libyaml-dev \
-  postfix \
-  libicu-dev \
-  libfreetype6 \
-  libfontconfig1 \
-  python-software-properties \
-  libfreetype6 \
-  sudo \
-  tree
+  sudo apt-get install -y "$list_apt"
   #fortune-mod \
   #cowsay
 
