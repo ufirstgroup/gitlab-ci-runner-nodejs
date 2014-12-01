14be8c28343ccc9c582c0990bc9152542d4c84a6
brownman <brownman556@gmail.com>
2014-12-01 16:31:47 +0200

    Update sudoers.sh

--- DKR/sudoers.sh
+++ DKR/sudoers.sh
@@ -1,5 +1,5 @@
 #ALLOW USING SUDO
 apt-get update -y
 apt-get install sudo
-useradd docker && echo "docker:docker" | chpasswd && adduser docker sudo
-mkdir -p /home/docker && chown -R docker:docker /home/docker
+useradd $user_docker && echo "${user_docker}:${user_docker}" | chpasswd && adduser $user_docker sudo
+mkdir -p /home/$user_docker && chown -R $user_docker:$user_docker /home/$user_docker
