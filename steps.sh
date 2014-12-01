196a6834dcbd3d60e2e1d006ebb0c7f0920f052d
brownman <brownman556@gmail.com>
2014-12-01 15:19:55 +0200

    split code from steps.txt

--- steps.sh
+++ steps.sh
@@ -1,4 +1,10 @@
 #ADD: copies new files, directories or remote file URLs from <src> and adds them to the filesystem of the container at the path <dest>. 
+
+
+trace(){
+ echo 1>&2 "[.] $@"
+}
+
  print_line(){
   echo 1>&2 '---------------------------------'
  }
@@ -20,9 +26,13 @@ breaking(){
  break
 }
 
-run(){
-local desc cmd
- 
+
+single(){
+local desc cmd str file
+str=$1
+file="$DIR_FIRST/STEPS/${str}.txt"
+test -f $file || { echo no file; exit 1; }
+
 while read line0;do
  test -n "$line0" || breaking
  line=$( echo $line0 | cut -d'#' -f1 )
@@ -33,8 +43,18 @@ while read line0;do
  echo "[$desc] $details"
  set +e
  commander "$cmd" 
-done < <(cat steps.txt)
+done < <( cat $file )
 }
 
-run
+all(){
+local str
+while read line;do
+ str=$( echo $line  | tr  '[a-z]'   '[A-Z]' )
+ commander single "$str" 
+done < <(cat $DIR_FIRST/steps.txt)
+}
+
+
+
+all
 exit 0
